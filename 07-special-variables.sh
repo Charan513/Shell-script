596fce052718c199d824191281db1d30f8deff94
Charan513 <charankandukuri9@gmail.com>
2025-02-05 08:34:32 +0530

    Script script

---
+++ 07-special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Which user is running this script:$USER"
+echo "Home drectory of current user: $HOME"
+echo "process id of current script: $$""
+sleep 60 &
+echo "process id of last commmand in background: $!"
