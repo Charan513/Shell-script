96554dd389868d4b99d0ef3ef2517d7564af4b9c
Charan513 <charankandukuri9@gmail.com>
2025-02-05 08:14:34 +0530

    Script script

---
+++ 06-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+MOVIES=("Pushpa" "RRR" "Devara")
+
+echo "First Movie: ${MOVIES[0])}"
+echo "Secound Movie: ${MOVIES[1]}"
+echo "Thrird Movie: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
